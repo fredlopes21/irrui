9ec8efdb6c6b28ee5a083d7d1c906dcb3e002084
Frederico <fredlopes21@me.com>
2022-04-06 14:12:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function loadCamera(){
 	if (navigator.mediaDevices.getUserMedia) {
 		navigator.mediaDevices.getUserMedia({audio: false, video: {facingMode: 'user'}})
 		.then( function(stream) {
-			//Definir o elemento vídeo a carregar o capturado pela webcam
+			//Definir o elemento víde a carregar o capturado pela webcam
 			video.srcObject = stream;
 		})
 		.catch(function(error) {
@@ -30,7 +30,7 @@ function takeSnapShot(){
 	canvas.height = video.videoHeight;
 	var ctx = canvas.getContext('2d');
 	
-	//Desenhando e convertendo as dimensões
+	//Desnehando e convertendo as minensões
 	ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 	
 	//Criando o JPG
@@ -40,34 +40,38 @@ function takeSnapShot(){
 	sendSnapShot(dataURI); //Gerar Imagem e Salvar Caminho no Banco
 }
 
-function sendSnapShot(base64){
-var request = new XMLHttpRequest();
-request.open('POST', 'save_photos.php', true);
-request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-<pre><code>    request.onload = function() {
-        console.log(request);
-        if (request.status >= 200 && request.status < 400) {
-            //Colocar o caminho da imagem no SRC
-            var data = JSON.parse(request.responseText);
+function sendSnapShot(base64){	
+	var request = new XMLHttpRequest();
+		request.open('POST', 'save_photos.php', true);
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		
+		request.onload = function() {
+			console.log(request);
+			if (request.status >= 200 && request.status < 400) {
+				//Colocar o caminho da imagem no SRC
+				var data = JSON.parse(request.responseText);
+				
+				//verificar se houve erro
+				if(data.error){
+					alert(data.error);
+					return false;
+				}
+				
+				//Mostrar informações
+				document.querySelector("#imagemConvertida").setAttribute("src", data.img);
+				document.querySelector("#caminhoImagem a").setAttribute("href", data.img);
+				document.querySelector("#caminhoImagem a").innerHTML = data.img.split("/")[1];
+			} else {
+				alert( "Erro ao salvar. Tipo:" + request.status );
+			}
+		};
+		
+		request.onerror = function() {
+		 	alert("Erro ao salvar. Back-End inacessível.");
+		}
+		
+		request.send("base_img="+base64); // Enviar dados
+}
 
-            //verificar se houve erro
-            if(data.error){
-                alert(data.error);
-                return false;
-            }
 
-            //Mostrar informações
-            document.querySelector("#imagemConvertida").setAttribute("src", data.img);
-            document.querySelector("#caminhoImagem a").setAttribute("href", data.img);
-            document.querySelector("#caminhoImagem a").innerHTML = data.img.split("/")[1];
-        } else {
-            alert( "Erro ao salvar. Tipo:" + request.status );
-        }
-    };
-
-    request.onerror = function() {
-        alert("Erro ao salvar. Back-End inacessível.");
-    }
-
-    request.send("base_img="+base64); // Enviar dados</code></pre>
-<p>}
+loadCamera();
