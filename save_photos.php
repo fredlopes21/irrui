92c6fffdace0e409f9e3b30eb02f3d73e8cdbf7a
Frederico <fredlopes21@me.com>
2022-04-06 15:57:28 -0300

    Update save_photos.php

--- save_photos.php
+++ save_photos.php
@@ -7,9 +7,9 @@
 
 	$result = [];
 	$data = str_replace(" ","+",$_POST['base_img']); //O envio do dado pelo XMLHttpRequest tende a trocar o + por espaço, por isso a necessidade de substituir. 
-	//$name = md5(time().uniqid());
-	$name = $_POST['base_img'];
-	$path = "snaps/{$name}.jpg";
+	$name = md5(time().uniqid());
+	$photo = $_POST['codaluno'];
+	$path = "snaps/{$photo}.jpg";
 
 	//data
 	$data = explode(',', $data);
