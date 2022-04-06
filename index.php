0a19e0ca4b291b602ee85be97660f535da5078ff
Frederico <fredlopes21@me.com>
2022-04-06 14:10:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-   <head>
-       <meta charset="utf-8"/>
-       <title>Entendo a estrutura e semântica do HTML5</title>
-   <link rel="stylesheet" type="text/css" href="style.css">
-	   <script src="script.js"></script>
 
-   </head>
-   <body>
-	   
-	   <div class="area">
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<!--Título-->
+		<title>Camera Test</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Apenas um teste"/>
+			
+		<!--OpenType-->
+		<meta property="og:locale" content="pt_BR" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="" />
+		<meta property="og:description" content="" />
+		<meta property="og:url" content="" />
+		<meta property="og:site_name" content="" />
+		
+		<!--CSS-->
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div class="area">
 			<video autoplay="true" id="webCamera">
 			</video>
-	
-			<input type="text" id="base_img" name="base_img">
-			<button type="button" onclick="takeSnapShot()">Tirar foto e salvar</button>
-	
-			<img id="imagemConvertida">
-			<p id="caminhoImagem" class="caminho-imagem"><a href="" target="_blank" rel="noopener"></a></p>
+			<form target="POST">
+				<textarea  type="text" id="base_img" name="base_img"></textarea>
+				<button type="button" onclick="takeSnapShot()">Tirar foto e salvar</button>
+			</form>
+			<img id="imagemConvertida"/>
+			<p id="caminhoImagem" class="caminho-imagem"><a href="" target="_blank"></a></p>
+			<!--Scripts-->
 			<script src="script.js"></script>
-</div>
-
-   </body>
+		</div>
+	</body>
 </html>
-
-
