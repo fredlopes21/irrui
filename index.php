86ffab9d3b83192b61505b9b1fec6f17592fa374
Frederico <fredlopes21@me.com>
2022-04-06 09:45:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-teste de site em php no heroku
+<div class="area">
+			<video autoplay="true" id="webCamera">
+			</video>
+	
+			<input type="text" id="base_img" name="base_img">
+			<button type="button" onclick="takeSnapShot()">Tirar foto e salvar</button>
+	
+			<img id="imagemConvertida">
+			<p id="caminhoImagem" class="caminho-imagem"><a href="" target="_blank" rel="noopener"></a></p>
+			<script src="script.js"></script>
+</div>
