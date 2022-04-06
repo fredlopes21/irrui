4b8326a41a14a622d03784b0d703d43056e21679
Frederico <fredlopes21@me.com>
2022-04-06 10:04:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!doctype html>
-<html lang="pt">
-<head>
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
- <title>Foto Carteirinha</title>
-</head>
-<body>
- <input type="file" accept="image/*" capture="camera">
-</body>
-</htm>
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+       <meta charset="utf-8"/>
+       <title>Foto Carteirinha</title>
+   </head>
+   <body>
+    <input type="file" accept="image/*" capture="camera">
+   </body>
+</html>
